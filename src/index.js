fabdc05444d79d3d222ea24ce3b4ebe58b574dd4
Nikhar Tale <nikhartale12@gmail.com>
2026-06-06 21:12:45 +0530

    Fix: Increase express JSON/urlencoded payload limit to 50mb to match gateway

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 34567;
 
 // Setup Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Logger Middleware to capture incoming taps and outgoing responses
 app.use((req, res, next) => {
